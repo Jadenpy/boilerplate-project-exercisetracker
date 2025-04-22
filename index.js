9d4038cc602ed85cf8c485c43464c2327b783423
virtual PC <huxinsheng2015@icloud.com>
2025-04-22 14:28:56 +0800

    整理了项目要求

--- index.js
+++ index.js
@@ -9,6 +9,43 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// ===========================My codes==================================================
+
+// section 1 : the requirements of the proj.
+
+// 1-1 :  your own proj.
+// 1-2 :  method: POST --> /api/users to create a new user.                                               // USER
+//        1-2-1 : check if the username is taken.
+//        1-2-2 : return a object {username: _id: }.
+// 1-3 :  method: GET  /api/users <-- all users in a list.
+// 1-4 :  method: POST --> /api/users/:_id/exercises to add an exercise.                                  // EXERCISE
+//        form items: description, duration, date(if empty, use today's date).
+//        return a user object added exercise property.
+// 1-5 :  method: GET  /api/users/:_id/logs to get a user's exercise log.                                 // USER EXERCISE LOG
+//        return a user object with added count and(number) logs(list) property.
+//        each log object should have description(string), duration(number), and date(string) properties.
+//        can add from and to query parameters to filter the logs by date.
+//        can add limit query parameter to limit the number of logs returned.
+
+
+
+
+
+
+
+
+
+// section 2 : the codes of the proj.
+
+// 2-1 :  declare the variables and the functions.
+// 2-2 :  the logic of the proj.
+
+
+
+
+
+
+// ===========================My codes==================================================
 
 
 
