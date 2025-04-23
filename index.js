3949f9368e457809acb27108f47fee2b683ed903
virtual PC <huxinsheng2015@icloud.com>
2025-04-23 08:32:28 +0800

    部分实现1-5的功能

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-app.use(bodyParse.urlencoded({ extended: false }));   // use body-parser   
+app.use(bodyParse.urlencoded({ extended: false }));   // use body-parser
 
 // ===========================My codes==================================================
 
@@ -19,16 +19,36 @@ app.use(bodyParse.urlencoded({ extended: false }));   // use body-parser
 // 1-2 :  method: POST --> /api/users to create a new user.                                               // USER
 //        1-2-1 : check if the username is taken.
 //        1-2-2 : return a object {username: _id: }.
+//        {
+//          username: "fcc_test",
+//          _id: "5fb5853f734231456ccb3b05"
+//        }
 // 1-3 :  method: GET  /api/users <-- all users in a list.
 // 1-4 :  method: POST --> /api/users/:_id/exercises to add an exercise.                                  // EXERCISE
 //        form items: description, duration, date(if empty, use today's date).
 //        return a user object added exercise property.
+//        {
+//          username: "fcc_test",
+//          description: "test",
+//          duration: 60,
+//          date: "Mon Jan 01 1990",
+//           _id: "5fb5853f734231456ccb3b05"
+//        }
 // 1-5 :  method: GET  /api/users/:_id/logs to get a user's exercise log.                                 // USER EXERCISE LOG
 //        return a user {} with added count(number),logs(list) property.
 //        each log {} should have description(string), duration(number), and date(string) properties.
 //        can add from and to query parameters to filter the logs by date.
 //        can add limit query parameter to limit the number of logs returned.
-
+//        {
+//          username: "fcc_test",
+//          count: 1,
+//          _id: "5fb5853f734231456ccb3b05",
+//          log: [{
+//           description: "test",
+//           duration: 60,
+//           date: "Mon Jan 01 1990",
+//          }]
+//        }
 
 
 
@@ -117,8 +137,12 @@ app.post("/api/users/:_id/exercises", (req, res) => {
     if (!req.body.date) {
       // use today's date
       date = new Date();//.toDateString();
-      // format it '2025-04-30'
-      date = date.toISOString().split('T')[0];
+      // format date "Mon Jan 01 1990"
+      date = date.toDateString();
+      // console.log('the date you input is not found, so use today\'s date', date);
+      // return;
+
+      // date = date.toISOString().split('T')[0];
       // console.log('the date you input is not found, so use today\'s date', date);
       // return;
     } else {
@@ -139,8 +163,36 @@ app.post("/api/users/:_id/exercises", (req, res) => {
 })
 
 
-// 1-5    
- TODO: how to delete all row including 'console.log'?
+// 1-5
+//  TODO: how to delete all row including 'console.log'?
+app.get("/api/users/:_id/logs", (req, res) => {
+  // get the _id from the url
+  let _id = req.params._id;
+  // console.log('the _id in paras is : ', _id);
+  // search the user in the users array
+  let user = users.find(user => user._id === parseInt(_id));
+  // check if the user is found
+  if (!user) {
+    // response err
+    res.json({ error: 'username not found' });
+    // console.log('the username is of the _id you input is not found');
+    // return;
+  } else {
+    // get the from and to date
+    // let from = req.query.from;
+    // let to = req.query.to;
+    // let limit = req.query.limit;
+    // console.log('the from and to date is : ', from, to);
+    // console.log('the limit is : ', limit);
+    // filter the exercises by the from and to date
+    let count = user.exercises.length;
+    let logs = user.exercises;
+    let resData = { _id: parseInt(_id), username: user.username, count, logs };
+    res.json(resData);
+    // console.log('the exercises of this user :' , user.exercises);
+    
+  }
+})
 
 
 
